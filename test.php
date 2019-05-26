88349e0c7ae93fc3adf145eb5d28341130d8c949
marcianok23 <36556298+marcianok23@users.noreply.github.com>
2019-05-26 13:43:14 -0500

    Update test.php

--- test.php
+++ test.php
@@ -1,8 +1,5 @@
 <?php
 return [
-    'subject' => [
-        'prefix' => '[Contact Form]'
-    ],
     'emails' => [
         'to'   => 'info@kylemarciano.com',
         'from' => 'info@kylemarciano.com'
@@ -10,13 +7,4 @@ return [
     'messages' => [
         'error'   => 'There was an error sending your message, please try again later.',
         'success' => 'Your message has been sent successfully.'
-    ],
-    'fields' => [
-        'name'     => 'Name',
-        'email'    => 'Email',
-        'phone'    => 'Phone',
-        'subject'  => 'Subject',
-        'message'  => 'Message',
-        'btn-send' => 'Send'
-    ]
 ];"
