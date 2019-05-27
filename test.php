41e6883371cf13a542cc78dc23d92462a7bd7f86
marcianok23 <36556298+marcianok23@users.noreply.github.com>
2019-05-27 18:00:54 -0500

    Update test.php

--- test.php
+++ test.php
@@ -1,8 +1,8 @@
 <?php
 return [
     'Submit' => [
-        'to'   => 'info@kylemarciano.com',
-        'from' => 'info@kylemarciano.com'
+        'to'   => 'marcianok23@gmail.com',
+        'from' => 'just10timberlakerzz@gmail.com'
     ],
     'messages' => [
         'error'   => 'There was an error sending your message, please try again later.',
